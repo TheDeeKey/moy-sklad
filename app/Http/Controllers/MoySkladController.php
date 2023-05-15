08bb42ac927213fea4dfcc46f355d919b29a064a
Artyom Klim <artyomklim001@gmail.com>
2023-05-15 22:05:36 +0500

    add: product remnants

--- app/Http/Controllers/MoySkladController.php
+++ app/Http/Controllers/MoySkladController.php
@@ -17,27 +17,24 @@ class MoySkladController extends Controller
 
     public function getAllData(Request $request)
     {
+        //TODO что если придет строка
+        $limit = $request->query('limit') ?? 1000;
+        $offset = $request->query('offset') ?? 0;
         session(['bearer_token' => $request->bearerToken()]);
 
-        $productResource = ProductResource::collection($this->toJson($request->bearerToken(), '/product'));
-        $productFolderResource = CategoryResource::collection($this->toJson($request->bearerToken(), '/productfolder'));
-        $storeResource = StoreResource::collection($this->toJson($request->bearerToken(), '/store'));
-        $retailstoreResource = RetailstoreResource::collection($this->toJson($request->bearerToken(), '/retailstore'));
-        $customerorderResource = CustomerOrderResource::collection($this->toJson($request->bearerToken(), '/customerorder'));
-        $uomResource = UomResource::collection($this->toJson($request->bearerToken(), '/uom'));
-
         return [
-            'product' => $productResource,
-            'product_folder' => $productFolderResource,
-            'store' => $storeResource,
-            'retailstore' => $retailstoreResource,
-            'customerorder' => $customerorderResource,
-            'uom' => $uomResource
+            'product' => ProductResource::collection($this->toJson($request->bearerToken(), '/product?limit=' . $limit . '&offset=' . $offset)),
+//            'product_folder' => CategoryResource::collection($this->toJson($request->bearerToken(), '/productfolder?limit=' . $limit . '&offset=' . $offset)),
+//            'store' => StoreResource::collection($this->toJson($request->bearerToken(), '/store?limit=' . $limit . '&offset=' . $offset)),
+//            'retailstore' => RetailstoreResource::collection($this->toJson($request->bearerToken(), '/retailstore?limit=' . $limit . '&offset=' . $offset)),
+//            'customerorder' => CustomerOrderResource::collection($this->toJson($request->bearerToken(), '/customerorder?limit=' . $limit . '&offset=' . $offset)),
+//            'uom' => UomResource::collection($this->toJson($request->bearerToken(), '/uom?limit=' . $limit . '&offset=' . $offset))
         ];
     }
 
-    private function toJson ($token,$endpoint) {
-        $list = Http::withToken($token)->get($this->base_url . '' .$endpoint);
+    private function toJson($token, $endpoint)
+    {
+        $list = Http::withToken($token)->get($this->base_url . '' . $endpoint);
         $listToJson = json_decode($list);
 
         return $listToJson->rows;
--- app/Http/Resources/MoySklad/ProductResource.php
+++ app/Http/Resources/MoySklad/ProductResource.php
@@ -11,35 +11,47 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @return array<string, mixed>
+     * @return array|false|mixed[]|string
      */
-    public function toArray(Request $request): array
+    public function toArray(Request $request)
     {
+        // Категория (группа) товара
         //example href: https://online.moysklad.ru/api/remap/1.2/entity/productfolder/be9f8eb7-ef67-11ed-0a80-08600000f992
         $parentIdInitialString = $this->productFolder->meta->href ?? '';
         $parent_id = '';
 
+        // Если категория есть, то извлекается её id из ссылки в объекте meta
         if ($parentIdInitialString != '') {
             $parent_id = substr($parentIdInitialString, strlen("https://online.moysklad.ru/api/remap/1.2/entity/productfolder/"));
         }
 
-        $photosGet = Http::withToken(session()->get('bearer_token'))->get($this->images->meta->href);
-        $photosJson = json_decode($photosGet);
-        $photosResource = ProductImagesResource::collection($photosJson->rows);
+        // Массив изображений товара
+        $photos = json_decode(Http::withToken(session()->get('bearer_token'))->get($this->images->meta->href));
+
+        // Массив остатков товаров
+        $stock_array = json_decode(Http::withToken(session()->get('bearer_token'))->get('https://online.moysklad.ru/api/remap/1.2/report/stock/all/current/'));
+        $stock_element_value = 0;
+
+        foreach ($stock_array as $element) {
+            if ($this->id === $element->assortmentId) {
+                $stock_element_value = $element->stock;
+            }
+        }
 
         return [
             'id' => $this->id,
             'name' => $this->name,
             'price' => $this->salePrices[0]->value,
+            'count' => $stock_element_value,
             'priceold' => 0,
-            'photo' => $photosResource,
-            'text' => $this->description,
+            //TODO ОБъект изображения, а не массив
+            'photo' => ProductImagesResource::collection($photos->rows),
+            'text' => $this->description ?? '',
             'position' => 'desc', // ?
             'parentid' => $parent_id,
             'id_crm' => $this->article ?? '',
             'view' => true,
             'withproduct' => '',
-            'count' => 0,
             'viewvariation' => $this->variantsCount > 0,
             'isvariant' => false,
             'productid' => 0,
